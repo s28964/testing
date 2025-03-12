adaabe505d471281f8d770e660d6e84dd3935d94
s28964 <s28964@pjwstk.edu.pl>
2025-03-12 19:15:25 +0100

    rednia w feature1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        double average = CalculatingAverage(numbers);
+        Console.WriteLine($"Średnia: {average}");
+    }
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+    static double CalculatingAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica jest pusta");
+        }
+
+        return numbers.Average();
+    }
+}
+
